763dec69f3d222a5ff4d4904b358c872f019528d
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-20 00:15:23 +0300

    Question and solution is reviewed

--- Quiz1/findNthMinimum.java
+++ Quiz1/findNthMinimum.java
@@ -2,18 +2,27 @@ package Quiz1;
 
 public class findNthMinimum {
 
-    // Soru sanıyorum ki en küçük n'inci sayıyı bulmak
-    // Aynen :D
+    // Find the nth minimum number of given array.
 
     public static void main(String[] args) {
+        // get the index input
+        int findMinIndex = Integer.parseInt(args[0]); 
+
+        // check if the index is valid
+        if (findMinIndex > args.length -1){
+            // inform the user
+            System.out.println("The given number array is not large enough !! Please try again...");
+            return;
+        }
+        
 
         // initialize 2 arrays
             // numbers hold the numbers
             // orders holds the positions
-            int[] numbers   = new int[args.length];
-            int[] orders    = new int[args.length];
-            int findMinIndex = Integer.parseInt(args[0]); 
-    
+            int[] numbers   = new int[args.length-1];
+            int[] orders    = new int[args.length-1];
+            
+            int NthMinNumber = 0;
              // iterate all numbers
              for(int i = 0 ; i < args.length -1; i++) {
                 // Parse it to double
@@ -27,13 +36,13 @@ public class findNthMinimum {
     
             // iterate numbers time to order them
             // each time point the minimum and assign an order
-            for(int i = 0 ; i < args.length ; i++) {
+            for(int i = 0 ; i < numbers.length ; i++) {
                 // initialize min and index
                 int nextMin = 9999999;
                 int nextMinIndex = 0;
                 // iterate all numbers again
                 // aim is finding the next minimum
-                for(int j = 0 ; j < args.length ; j++) {
+                for(int j = 0 ; j < numbers.length ; j++) {
                     // the next min number shouldn't be assigned a value
                     if(orders[j] == 0){
                         // if the current number is less or equal to the min
@@ -52,8 +61,14 @@ public class findNthMinimum {
                 order++;
             }
 
+            
+            for (int i = 0 ; i < orders.length ; i ++){
+                if (orders[i] == findMinIndex){
+                    NthMinNumber = numbers[i]; // first number is ignored
+                }
+            }
             // get the number
-            int NthMinNumber = Integer.parseInt(args[findMinIndex+1]); // first number is ignored
+            
             System.out.println(findMinIndex + "th minimum number is " + NthMinNumber);
     }   
     
