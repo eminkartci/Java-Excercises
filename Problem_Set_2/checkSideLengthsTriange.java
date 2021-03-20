7b166b42aa4821ca1e70638d705dda7cf955edd9
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-20 21:32:05 +0300

    check input is added

--- Problem_Set_2/checkSideLengthsTriange.java
+++ Problem_Set_2/checkSideLengthsTriange.java
@@ -1,11 +1,34 @@
 package Problem_Set_2;
 
+/*
+Question 2 
+
+Write a Java program to check if three given side lengths (integers) can
+make a triangle or not.
+Expected Output: 
+
+Input side1: 5 
+Input side2: 6 
+Input side3: 8
+
+Is the said sides form a triangle: true
+
+*/
+
+
 public class checkSideLengthsTriange {
 
     public static void main(String[] args){
 
+        check_inputs(args);
         double[] numbers = convertDouble(args);
         print_double_array(numbers);
+    }
+
+    public static void check_inputs(String[] input){
+        if(input.length != 3){
+            System.out.println("Your input is not valid !! ");
+        }
     }
 
     public static double[] convertDouble(String[] input){
