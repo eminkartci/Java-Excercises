3d5df16507c46d48d9cd5d9945a6d7465322c979
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-21 21:09:38 +0300

    Quiz 4 - Question 2 : Person Code Improving / Constructor is added

---
+++ Quiz4/Person.java
@@ -0,0 +1,13 @@
+package Quiz4;
+
+public class Person {
+
+    private int height;
+    private int weight;
+
+    public Person(int height, int weight){
+        this.height = height;
+        this.weight = weight;
+    }
+    
+}
