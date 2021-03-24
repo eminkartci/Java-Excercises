d939e8810193a7cfdabf2972a48cab6b205f24f9
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-24 20:41:15 +0300

    Homework 2 - Question 3 / Detect Twice Elements

---
+++ Homework2/TwiceElement.java
@@ -0,0 +1,13 @@
+package Homework2;
+
+/*
+
+10.Given an array of integers,
+write a method that returns true if the array repeats a number 
+"exactly" twice somewhere, and returns false otherwise. 
+
+*/
+
+public class TwiceElement {
+    
+}
