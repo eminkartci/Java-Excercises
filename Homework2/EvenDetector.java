6138b3bdb5f6c2a6524a3a870467ea52c1356d92
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-24 20:39:55 +0300

    Count even numbers method is implemented - Main method is implemented

--- Homework2/EvenDetector.java
+++ Homework2/EvenDetector.java
@@ -10,14 +10,28 @@ public class EvenDetector {
 
     public static void main(String[] args){
 
+        // initialize numbers array
+        int[] dinazor = {5,7,9,2,6,8};
+        // call the method
+        print_array(detect_even_integers(dinazor));
         
+    }
+
+    // get an array
+    public static void print_array(int[] arr){
+        // iterate all items
+        for ( int x : arr ){
+            // print one by one
+            System.out.print(x+ " ");
+        }
+        System.out.println();
     }
 
     // 1 Parameter 
         // Int Array
     public static int[] detect_even_integers(int[] numbers){
 
-        int[] evenNumbers = new int[numbers.length];
+        int[] evenNumbers = new int[count_even_numbers(numbers)];
         int index = 0;
         // iterate all nubber
         for(int number : numbers){
@@ -32,6 +46,21 @@ public class EvenDetector {
         // return even numbers
         return evenNumbers;
 
+    }
+
+    public static int count_even_numbers(int[] numbers){
+        
+        int evenCount = 0;
+        // iterate all nubber
+        for(int number : numbers){
+            // check if the number is even
+            if(number % 2 == 0){
+                // increase the count
+                evenCount++;
+            }
+        }
+        // return even numbers
+        return evenCount;
     }
     
 }
