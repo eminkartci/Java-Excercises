003465f4646216f602d2d372c899b5daff29f336
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-25 22:09:34 +0300

    return_twice_elements method is implemented

--- Homework2/TwiceElement.java
+++ Homework2/TwiceElement.java
@@ -2,12 +2,51 @@ package Homework2;
 
 /*
 
-10.Given an array of integers,
+10. Given an array of integers,
 write a method that returns true if the array repeats a number 
 "exactly" twice somewhere, and returns false otherwise. 
 
 */
 
 public class TwiceElement {
+
+
+
+    // This method returns elements of they repeats exactly twice
+        // Input -> Integer Array
+    public static boolean return_twice_elements(int[] numbers){
+        // initialize a number
+        int count = 0;
+        // initialize 
+        // iterate all numbers
+        for (int i = 0 ; i < numbers.length ; i++){
+            
+            // check if they repeat
+            for (int j = 0 ; j < numbers.length ; j++){
+
+                // reset the count
+                count = 0;
+
+                // check all numbers
+                if(numbers[i] == numbers[j]){
+                    // increase the count
+                    count ++;
+                }
+
+            }
+
+            // If this numbers iterates exactly twice
+            if(count == 2){
+                return true;
+            }
+
+        }
+        
+        // otherwise
+        return false;
+
+    }
     
 }
+
+
