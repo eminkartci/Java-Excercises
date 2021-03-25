5e05190c41b37b9dc52d34d7dc3917a182ea4a70
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-25 22:29:53 +0300

    Step 2 - Connect chars and row count / Set equations / Set for loops / Main Method

--- Extra_Questions/DrawStars.java
+++ Extra_Questions/DrawStars.java
@@ -9,43 +9,56 @@ Q1 - Draw this:
  ******   ******
 ******** ********
 
+Row     0 1 2 3
+space1  3 2 1 0
+star1   2 4 6 8
+space2  7 5 3 1
+star2   2 4 6 8
 */
 public class DrawStars {
 
 
+    public static void main(String args[]){
+        draw_Q1();
+    }
 
     public static void draw_Q1(){
 
         // Constants
-        int rowCount = 4;
+        int rowCount        = 4;
+        int firstSpace      = 3;
+        int initialStar     = 2;
+        int secindSpace     = 7;
 
         // iteare rows
         for(int row = 0 ; row < rowCount ; row++){
 
-            // Spaces
-            for(int space = 0 ; space < 3 ; space++){
+            // Spaces 1
+            for(int space = 0 ; space < firstSpace - row; space++){
 
                 System.out.print(" ");
 
             }
 
-            // Stars
-            for (int star = 0 ; star < 2 ; star ++){
+            // Stars 1
+            for (int star = 0 ; star < initialStar + (initialStar * row) ; star ++){
                 System.out.print("*");
             }
 
-            // Spaces
-            for(int space = 0 ; space < 7 ; space++){
+            // Spaces 2
+            for(int space = 0 ; space < secindSpace - (row * 2) ; space++){
 
                 System.out.print(" ");
 
             }
 
-            // Stars
-            for (int star = 0 ; star < 2 ; star ++){
+            // Stars 2
+            for (int star = 0 ; star < initialStar + (initialStar * row) ; star ++){
                 System.out.print("*");
             }
 
+            // next line
+            System.out.println();
         }
     }
     
