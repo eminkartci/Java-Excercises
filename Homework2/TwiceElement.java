be24b4d3b0da1515d57dad5e688a770b8a5bfcf6
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-27 22:23:17 +0300

    Question Description Reviewved

--- Homework2/TwiceElement.java
+++ Homework2/TwiceElement.java
@@ -2,7 +2,7 @@ package Homework2;
 
 /*
 
-10. Given an array of integers,
+HW2 - Q3 Given an array of integers,
 write a method that returns true if the array repeats a number 
 "exactly" twice somewhere, and returns false otherwise. 
 
