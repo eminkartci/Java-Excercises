250dc3596e4092e1677dfc9b642c7bf9316e4bd7
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-01 21:27:44 +0300

    Count Even Numbers Only

--- Homework2/EvenDetector.java
+++ Homework2/EvenDetector.java
@@ -13,39 +13,8 @@ public class EvenDetector {
         // initialize numbers array
         int[] dinazor = {5,7,9,2,6,8};
         // call the method
-        print_array(detect_even_integers(dinazor));
+        System.out.println(count_even_numbers(dinazor));
         
-    }
-
-    // get an array
-    public static void print_array(int[] arr){
-        // iterate all items
-        for ( int x : arr ){
-            // print one by one
-            System.out.print(x+ " ");
-        }
-        System.out.println();
-    }
-
-    // 1 Parameter 
-        // Int Array
-    public static int[] detect_even_integers(int[] numbers){
-
-        int[] evenNumbers = new int[count_even_numbers(numbers)];
-        int index = 0;
-        // iterate all nubber
-        for(int number : numbers){
-            // check if the number is even
-            if(number % 2 == 0){
-                // put the number in the result array
-                evenNumbers[index] = number;
-                // increase the index
-                index++;
-            }
-        }
-        // return even numbers
-        return evenNumbers;
-
     }
 
     public static int count_even_numbers(int[] numbers){
