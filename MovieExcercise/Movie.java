4a9d1470cf8a7c26f196d448d2897d1722192453
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-03 23:42:58 +0300

    Movie Selection Algorithm

--- MovieExcercise/Movie.java
+++ MovieExcercise/Movie.java
@@ -4,28 +4,67 @@ public class Movie {
 
     public static void main(String[] args) {
 
+        // Movie 1
         Movie m1 = new Movie("Leon: The Professional", 5,10);
         m1.getInfo();
-        System.out.println();
+
+        // Movie 2
         Movie m2 = new Movie ("Stockholm",2,20);
         m2.getInfo();
+
+        Movie.compare_movies(m1, m2);
     }
 
+    public static void compare_movies(Movie m1,Movie m2){
+
+        double ratioM1 = (double) m1.getRating() / (double) m1.getTicketPrice();
+        double ratioM2 = (double) m2.getRating() / (double) m2.getTicketPrice();
+
+        if (ratioM1 > ratioM2){
+            System.out.println("You should go to the "+ m1.getTitle());
+        }else if (ratioM1 == ratioM2){
+            System.out.println("Both films are OK!");
+        }
+        else {
+            System.out.println("You should go to the "+ m2.getTitle());
+        }
+    }
+
+    // Attributes
     private String title;
     private int rating;
     private int ticketprice;
 
-    //constructor
+    // Constructor
     public Movie(String t, int r,int p) {
          this.title=t;
          this.rating=r;
          this.ticketprice=p;
     }
 
+
     public void getInfo() {
-        System.out.println("Title is: "+this.title);
-        System.out.println("Rating is "+this.rating);
-        System.out.println("Ticket price is: "+this.ticketprice);
+
+        // String content
+        String content = "Title         : "+this.title      +"\n"
+                        +"Rating        : "+this.rating     +"\n"
+                        +"Ticket price  : "+this.ticketprice+"\n";
+
+        // Print
+        System.out.println(content);
+    }
+
+    // GETTERS
+    public int getRating(){
+        return this.rating;
+    }
+
+    public int getTicketPrice(){
+        return this.ticketprice;
+    }
+
+    public String getTitle(){
+        return this.title;
     }
 
 }
