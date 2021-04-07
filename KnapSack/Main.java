eb5e32f9a0597d9ea31bec24089865791bc88ef8
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-07 22:16:13 +0300

    Sinem's Question

--- KnapSack/Main.java
+++ KnapSack/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args){
         // define initial values
-        int[] weights   = {1,2,4,2,5};
-        int[] values    = {5,3,5,3,2};
+        int[] weights   = {5,4,6,3};
+        int[] values    = {10,40,30,50};
 
         // create object
         KnapSack problem1 = new KnapSack(weights,values);
         
         // get max value 
-        System.out.println(problem1.findMaxValue(4,10));
+        System.out.println(problem1.findMaxValue(3,10));
     }
 }
