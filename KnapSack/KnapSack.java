908313d0129cdee7f2387e13e4181268d70436ae
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-07 22:04:49 +0300

    Knap Sack Problem inital commit

---
+++ KnapSack/KnapSack.java
@@ -0,0 +1,18 @@
+package KnapSack;
+
+public class KnapSack {
+
+    private int[] weights,values;
+    private int maxCapacity;
+
+
+    public KnapSack(int[] weights,int[] values){
+        this.weights = weights;
+        this.values = values;
+    }
+
+    public void solveKnapSack(int itemIndex,int remainedCapacity){
+
+    }
+    
+}
