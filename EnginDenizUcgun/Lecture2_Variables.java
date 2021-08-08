e227d291ac5940a6d010e18feb62ed1b65b0ec99
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-08 15:51:46 +0300

    Print Rectangle

--- EnginDenizUcgun/Lecture2_Variables.java
+++ EnginDenizUcgun/Lecture2_Variables.java
@@ -7,7 +7,8 @@ public class Lecture2_Variables {
         Student student1 = new Student(21,"Deniz");
         Student student2 = new Student(22,"Emin"); 
         student2.friends[0] = student1;
-        introduce(student1);
+        //introduce(student1);
+        printRect(52,2);
 
     }
 
@@ -18,7 +19,19 @@ public class Lecture2_Variables {
     public static void introduce(Student s) {
         System.out.println(s.name + " is " + s.age + " years old.");
     }
-    
+
+    public static void printRect(int h, int w){
+
+        for(int k = 0 ; k < h ; k++){
+            for(int i =0 ; i < w ; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+    }
+
 }
 
 
