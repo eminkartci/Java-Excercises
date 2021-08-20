3dbe842892113c84576754f1712685639ae2b249
eminkartci <emin.kartci@ozu.edu.tr>
2021-08-20 22:13:28 +0300

    Simulasyon yapildi

--- EnginDenizUcgun/Lecture3_MonteCarlo.java
+++ EnginDenizUcgun/Lecture3_MonteCarlo.java
@@ -3,7 +3,19 @@ package EnginDenizUcgun;
 public class Lecture3_MonteCarlo{
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("Result: " + flipCoin());
+        
+    }
+
+    public static String flipCoin(){
+
+        if(Math.random() < 0.5){
+            return "T";
+        }else{
+            return "H";
+        }
+
     }
 
 }
